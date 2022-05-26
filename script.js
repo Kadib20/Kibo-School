193db6cfb4f383537ca05b26e462ad04082e134f
Chika Ndubuisi <>
2022-05-26 02:43:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var count = 0;
+var init = 7;
+
+const button = document.querySelector('button');
+//const #reset = document.querySelector('#reset');
+
+button.addEventListener('click', () => {
+
+document.querySelector('h2').textContent = count;
+count = count + 2;
+console.log("+1 clicked"); 
+});
+
+
+
+
+
+//button.addEventListener('click', () => {
+
+//document.querySelector('h2').classList.add('2').textContent = decrease;
+  
+//decrease = (count + 1) - 1;
+
+//});
+
+/* this will track the count */
+
+
+/* add your event listener here */
